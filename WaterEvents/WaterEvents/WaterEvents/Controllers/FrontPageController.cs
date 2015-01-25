2078dc0ce8ef6c7a121144d4a9d3c02b9c547597
Tjornfelt <madstjornfelt@gmail.com>
2015-01-25 14:20:24 +0100

    Init
    The main solution files, as well as basic setup + some work on frontpage
    + layout

---
+++ WaterEvents/WaterEvents/WaterEvents/Controllers/FrontPageController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web;
+using Umbraco.Core.Models;
+using WaterEvents.Models;
+
+namespace WaterEvents.Controllers
+{
+    public class FrontPageController : MasterController
+    {
+        //
+        // GET: /FrontPage/
+
+        public ActionResult FrontPage(RenderModel renderModel)
+        {
+            TestModel model = new TestModel()
+            {
+                Test = "asd"
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ WaterEvents/WaterEvents/WaterEvents/Controllers/MasterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Web;
+
+namespace WaterEvents.Controllers
+{
+    public class MasterController : RenderMvcController
+    {
+        protected override ViewResult View(string viewName, string masterName, object model)
+        {
+
+            //Move all "Global content" into the viewbag - it shouldnt be accessible in the model
+
+            var frontPage = CurrentPage.AncestorOrSelf(1);
+
+            return base.View(viewName, masterName, model);
+        }
+    }
+}
